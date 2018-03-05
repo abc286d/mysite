a28a5f6835b17f3f44fd71c7e55940b3262253f3
Leyan <605773683@qq.com>
2018-03-05 18:46:17 +0800

    Part 2.1.3 Complete

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,7 @@
 from django.urls import re_path,path
 from . import views
 
+app_name = "blog"
 urlpatterns = [
     re_path(r'^$', views.blog_title, name='blog_title'),
     path('<int:article_id>/', views.blog_article, name="blog_detail")
