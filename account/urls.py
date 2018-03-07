813b942bf7213ff24a5858f2d48fccfffa91233b
Leyan <605773683@qq.com>
2018-03-08 00:07:01 +0800

    Part 2.4 Complete

--- account/urls.py
+++ account/urls.py
@@ -3,7 +3,12 @@ from . import views
 
 from django.conf import settings
 
+from django.contrib.auth import views as auth_views
+
 app_name = "account"
 urlpatterns = [
-    path('login/', views.user_login, name="user_login")
+    #path('login/', views.user_login, name="user_login")
+    path('login/', auth_views.login, name='user_login'),
+    path('logout/', auth_views.logout, {'template_name': "account/logout.html"}, name='user_logout'),
+    path('register/', views.register, name="user_register")
 ]
