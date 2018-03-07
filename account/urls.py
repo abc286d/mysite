0b6e5888c2e1f238a81adfa1f1fd1493f9ffa257
Leyan <605773683@qq.com>
2018-03-08 01:20:39 +0800

    Part 2.5.1 Complete

--- account/urls.py
+++ account/urls.py
@@ -10,5 +10,8 @@ urlpatterns = [
     #path('login/', views.user_login, name="user_login")
     path('login/', auth_views.login, name='user_login'),
     path('logout/', auth_views.logout, {'template_name': "account/logout.html"}, name='user_logout'),
-    path('register/', views.register, name="user_register")
+    path('register/', views.register, name="user_register"),
+    path('password-change/', auth_views.password_change,
+         {"post_change_redirect": "/account/password-change-done/"}, name="password_change"),
+    path("password-change-done/", auth_views.password_change_done, name="password_change_done"),
 ]
