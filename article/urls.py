419fa55aa10a7d3452a88643545091b26f10d149
Leyan <605773683@qq.com>
2018-03-14 20:42:04 +0800

    Part 3.2 Complete

--- article/urls.py
+++ article/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     path('article-column/', views.article_column, name='article_column'),
     path('rename-column/', views.rename_article_column, name='rename_article_column'),
     path('del-column/', views.del_article_column, name='del_article_column'),
+    path('article-post/', views.article_post, name='article_post'),
+    path('article-list/', views.article_list, name='article_list'),
+    re_path(r'^article-detail/(?P<id>\d+)/(?P<slug>[-\w]+)/$', views.article_detail, name='article_detail'),
 ]
